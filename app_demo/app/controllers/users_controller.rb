16f387ad551ff56375cad4b775de3b763de063da
do.minh.truong <SUN-ASTERISK\do.minh.truong@i120928-pc.sun-asterisk.com>
2019-08-02 16:45:26 +0700

    refactor users_controller.rb

--- app_demo/app/controllers/users_controller.rb
+++ app_demo/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	skip_before_action :require_login, only: [:new, :create]
+
 	def index
 		@users = User.all
   	end
@@ -9,25 +10,23 @@ class UsersController < ApplicationController
 	end
 
 	def create
-	    @user = User.new user_params
-	    if @user.save
-	      flash[:success] = "Register success"
-	      redirect_to users_path
-	    else
-	      flash[:success] = "Register failed"
-	      render :new
-	    end
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = "Register success"
+      redirect_to users_path
+    else
+      flash[:success] = "Register failed"
+      render :new
     end
-    
-    def show
-	    @user = User.find_by id: params[:id]
-	  end
+  end
+  
+  def show
+    @user = User.find_by id: params[:id]
+  end
 
-    private
-    def user_params
-    	params.require(:user).permit :name, :password, :password_confirmation
-    end
+  private
 
-
-
+  def user_params
+  	params.require(:user).permit :name, :password, :password_confirmation
+  end
 end
